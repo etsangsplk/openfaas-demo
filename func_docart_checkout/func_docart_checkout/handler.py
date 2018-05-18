9d6c4e4495aafe3c74a23eafce243215cfa2cdc6
vivsridh4 <vsridhar@digitalocean.com>
2018-05-18 07:49:42 +0530

    never push so many changes

--- func_docart_checkout/func_docart_checkout/handler.py
+++ func_docart_checkout/func_docart_checkout/handler.py
@@ -1,7 +1,15 @@
+import os
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
 
-    return req
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, 'html','site','index.html')
+
+    with(open(path, 'r')) as file:
+        html = file.read()
+
+    return html
